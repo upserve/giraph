4b20e3a4ba3a6e3b902f6f537a090ec2e079706d
Erman Celen <erman@upserve.com>
2016-08-08 11:52:54 -0400

    Reminder for end-to-end integration test

--- spec/giraph_spec.rb
+++ spec/giraph_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe Giraph do
+  describe 'full integration' do
+    # TODO: A full, end-to-end integration test suite including:
+    # - Type definitions
+    # - Child: Schema declaration
+    # - Chile: Resolver declaration
+    # - Parent: Connected field declaration
+    # - Parent: Evaluator block
+    # - Request/Response
+  end
+
   it 'has a version number' do
     expect(Giraph::VERSION).not_to be nil
   end
